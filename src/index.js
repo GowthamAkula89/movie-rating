6bda7982f75fbd4b2e9d4cfbf9d4c49888e1ebbd
GowthamAkula89 <akulagowthamroyal@gmail.com>
2024-07-21 14:28:40 +0530

    Connect MongoDB Atlas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
+const mongoose = require('mongoose')
 require('dotenv').config();
 const app = require('./app');
-
 const PORT= process.env.PORT || 3001;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+const DB_URL = process.env.DB_URL;
+
+mongoose.connect(DB_URL)
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("Failed to connect db", err);
 })
